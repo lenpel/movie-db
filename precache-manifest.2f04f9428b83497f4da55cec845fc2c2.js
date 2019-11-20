f394b0791df9e2d8061346f1f20034df9dffa05c
lenpel <26303224+lenpel@users.noreply.github.com>
2019-11-20 17:49:09 +0100

    Updates

---
+++ precache-manifest.2f04f9428b83497f4da55cec845fc2c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d77b238800dee660fe83ff883bcca4c",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "0ceed1f3e887d193c73e",
+    "url": "/movie-db/static/css/main.e434abcb.chunk.css"
+  },
+  {
+    "revision": "445a9710edbc3f6cb866",
+    "url": "/movie-db/static/js/2.5ab534ab.chunk.js"
+  },
+  {
+    "revision": "0ceed1f3e887d193c73e",
+    "url": "/movie-db/static/js/main.a4907113.chunk.js"
+  },
+  {
+    "revision": "28bc7c70dc45fea09cc4",
+    "url": "/movie-db/static/js/runtime-main.84f1c5bc.js"
+  }
+]);
